b07a33d850aa71b81ec0fc10065e43ceaa5bf7ee
chenyue404 <601450225@qq.com>
2021-03-10 17:21:16 +0800

    Initial commit

---
+++ app/src/main/java/com/chenyue404/nojump/HookManager.kt
@@ -0,0 +1,26 @@
+package com.chenyue404.nojump
+
+import de.robv.android.xposed.IXposedHookLoadPackage
+import de.robv.android.xposed.callbacks.XC_LoadPackage
+
+object HookManager {
+
+    private val hookClassList by lazy(LazyThreadSafetyMode.NONE) {
+        hashSetOf<IXposedHookLoadPackage>()
+    }
+
+    fun startHook(lpparam: XC_LoadPackage.LoadPackageParam) {
+        hookClassList.forEach { it.handleLoadPackage(lpparam) }
+    }
+
+    fun registHookClass(hookerList: List<IXposedHookLoadPackage>) {
+        hookClassList.addAll(hookerList)
+    }
+
+    init {
+        registHookClass(
+            arrayListOf(
+            )
+        )
+    }
+}
