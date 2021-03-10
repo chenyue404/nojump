dfa8f072f6f3f69e23a131d7428a908e83b34c5b
chenyue404 <601450225@qq.com>
2021-03-10 22:29:03 +0800

    Other Android version

--- app/src/main/java/com/chenyue404/nojump/hook/JumpHook.kt
+++ app/src/main/java/com/chenyue404/nojump/hook/JumpHook.kt
@@ -1,8 +1,8 @@
 package com.chenyue404.nojump.hook
 
-import android.app.AndroidAppHelper
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.IBinder
@@ -32,72 +32,173 @@ class JumpHook : IXposedHookLoadPackage {
         val ProfilerInfo =
             XposedHelpers.findClass("android.app.ProfilerInfo", classLoader)
 
-        XposedHelpers.findAndHookMethod("com.android.server.am.ActivityManagerService",
-            classLoader,
-            "startActivityAsUser",
-            IApplicationThread,
-            String::class.java,
-            Intent::class.java,
-            String::class.java,
-            IBinder::class.java,
-            String::class.java,
-            Int::class.javaPrimitiveType,
-            Int::class.javaPrimitiveType,
-            ProfilerInfo,
-            Bundle::class.java,
-            Int::class.javaPrimitiveType,
-            object : XC_MethodHook() {
-                override fun beforeHookedMethod(param: MethodHookParam) {
-                    val intent = param.args[2] as Intent
-                    val targetActivity =
-                        if (intent.component == null) "" else intent.component!!
-                            .className
-                    val targetApp = intent.getPackage()
-                    val callingPackage = param.args[1] as String
-                    val dataString = intent.dataString
-                    val scheme = intent.scheme
-                    val currentPackageName = AndroidAppHelper.currentPackageName()
-                    val field = XposedHelpers.findFieldIfExists(
-                        param.thisObject.javaClass,
-                        "mUiContext"
-                    )
-                    val handlerField = XposedHelpers.findFieldIfExists(
-                        param.thisObject.javaClass,
-                        "mUiHandler"
-                    )
-                    if (field != null && !TextUtils.isEmpty(scheme)
-                        && scheme == "openapp.jdmobile"
-                    ) {
-                        try {
-                            val context = field[param.thisObject] as Context
-                            val mUiHandler = handlerField[param.thisObject] as Handler
-                            mUiHandler.post {
-                                XposedBridge.log(TAG + "执行Toast")
-                                Toast.makeText(
-                                    context,
-                                    "$TAG--$currentPackageName",
-                                    Toast.LENGTH_SHORT
-                                ).show()
-                            }
-                            param.result = 0
-                        } catch (e: IllegalAccessException) {
-                            XposedBridge.log(TAG + e.toString())
-                            e.printStackTrace()
+        val parameterTypesAndCallback = object : XC_MethodHook() {
+            override fun beforeHookedMethod(param: MethodHookParam) {
+                val intent = param.args[2] as Intent
+                val targetActivity =
+                    if (intent.component == null) "" else intent.component!!
+                        .className
+                val targetApp = intent.getPackage()
+                val callingPackage = param.args[1] as String
+                val dataString = intent.dataString
+                val scheme = intent.scheme
+                val field = XposedHelpers.findFieldIfExists(
+                    param.thisObject.javaClass,
+                    "mUiContext"
+                )
+                val handlerField = XposedHelpers.findFieldIfExists(
+                    param.thisObject.javaClass,
+                    "mUiHandler"
+                )
+                if (field != null && !TextUtils.isEmpty(scheme)
+                    && scheme == "openapp.jdmobile"
+                ) {
+                    try {
+                        val context = field[param.thisObject] as Context
+                        val mUiHandler = handlerField[param.thisObject] as Handler
+                        mUiHandler.post {
+                            XposedBridge.log(TAG + "执行Toast")
+                            Toast.makeText(
+                                context,
+                                "$TAG 执行Toast",
+                                Toast.LENGTH_SHORT
+                            ).show()
                         }
-                    } else {
-                        XposedBridge.log(TAG + "field为空")
+                        param.result = 0
+                    } catch (e: IllegalAccessException) {
+                        XposedBridge.log(TAG + e.toString())
+                        e.printStackTrace()
                     }
-                    XposedBridge.log(
-                        TAG + "--callingPackage=" + callingPackage
-                                + "--targetActivity=" + targetActivity
-                                + "--targetApp=" + targetApp
-                                + "--dataString=" + dataString
-                                + "--scheme=" + scheme
-                                + "--currentPackageName=" + currentPackageName
-                    )
+                } else {
+                    XposedBridge.log(TAG + "field为空")
                 }
+                XposedBridge.log(
+                    TAG + "--callingPackage=" + callingPackage
+                            + "--targetActivity=" + targetActivity
+                            + "--targetApp=" + targetApp
+                            + "--dataString=" + dataString
+                            + "--scheme=" + scheme
+                )
             }
-        )
+        }
 
+        when (Build.VERSION.SDK_INT) {
+            in Build.VERSION_CODES.M..Build.VERSION_CODES.O_MR1 -> {
+                XposedHelpers.findAndHookMethod(
+                    "com.android.server.am.ActivityManagerService",
+                    classLoader,
+                    "startActivityAsUser",
+                    IApplicationThread,
+                    String::class.java,
+                    Intent::class.java,
+                    String::class.java,
+                    IBinder::class.java,
+                    String::class.java,
+                    Int::class.java,
+                    Int::class.java,
+                    ProfilerInfo,
+                    Bundle::class.java,
+                    Int::class.java,
+                    parameterTypesAndCallback
+                )
+            }
+            Build.VERSION_CODES.P -> {
+                XposedHelpers.findAndHookMethod(
+                    "com.android.server.am.ActivityManagerService",
+                    classLoader,
+                    "startActivityAsUser",
+                    IApplicationThread,
+                    String::class.java,
+                    Intent::class.java,
+                    String::class.java,
+                    IBinder::class.java,
+                    String::class.java,
+                    Int::class.java,
+                    Int::class.java,
+                    ProfilerInfo,
+                    Bundle::class.java,
+                    Int::class.java,
+                    Boolean::class.java,
+                    parameterTypesAndCallback
+                )
+            }
+            Build.VERSION_CODES.Q -> {
+                XposedHelpers.findAndHookMethod(
+                    "com.android.server.wm.ActivityTaskManagerService",
+                    classLoader,
+                    "startActivityAsUser",
+                    IApplicationThread,
+                    String::class.java,
+                    Intent::class.java,
+                    String::class.java,
+                    IBinder::class.java,
+                    String::class.java,
+                    Int::class.java,
+                    Int::class.java,
+                    ProfilerInfo,
+                    Bundle::class.java,
+                    Int::class.java,
+                    Boolean::class.java,
+                    parameterTypesAndCallback
+                )
+            }
+            Build.VERSION_CODES.R -> {
+                XposedHelpers.findAndHookMethod(
+                    "com.android.server.wm.ActivityTaskManagerService",
+                    classLoader,
+                    "startActivityAsUser",
+                    IApplicationThread,
+                    String::class.java,
+                    String::class.java,
+                    Intent::class.java,
+                    String::class.java,
+                    IBinder::class.java,
+                    String::class.java,
+                    Int::class.java,
+                    Int::class.java,
+                    ProfilerInfo,
+                    Bundle::class.java,
+                    Int::class.java,
+                    Boolean::class.java,
+                    parameterTypesAndCallback
+                )
+            }
+        }
     }
+
+//23,24,25,26,27
+//    android.app.IApplicationThread
+//    android.app.ProfilerInfo
+//        com.android.server.am.ActivityManagerService
+//        startActivityAsUser(IApplicationThread caller, String callingPackage,
+//            Intent intent, String resolvedType, IBinder resultTo, String resultWho, int requestCode,
+//            int startFlags, ProfilerInfo profilerInfo, Bundle bOptions, int userId)
+
+//28
+//    android.app.IApplicationThread
+//    android.app.ProfilerInfo
+//        com.android.server.am.ActivityManagerService
+//        startActivityAsUser(IApplicationThread caller, String callingPackage,
+//            Intent intent, String resolvedType, IBinder resultTo, String resultWho, int requestCode,
+//            int startFlags, ProfilerInfo profilerInfo, Bundle bOptions, int userId,
+//            boolean validateIncomingUser)
+
+//29
+//    android.app.IApplicationThread
+//    android.app.ProfilerInfo
+//    com.android.server.wm.ActivityTaskManagerService
+//    startActivityAsUser(IApplicationThread caller, String callingPackage,
+//        Intent intent, String resolvedType, IBinder resultTo, String resultWho, int requestCode,
+//        int startFlags, ProfilerInfo profilerInfo, Bundle bOptions, int userId,
+//        boolean validateIncomingUser)
+
+//30
+//    android.app.IApplicationThread
+//    android.app.ProfilerInfo
+//    com.android.server.wm.ActivityTaskManagerService
+//    startActivityAsUser(IApplicationThread caller, String callingPackage,
+//        @Nullable String callingFeatureId,
+//        Intent intent, String resolvedType, IBinder resultTo, String resultWho, int requestCode,
+//        int startFlags, ProfilerInfo profilerInfo, Bundle bOptions, int userId,
+//        boolean validateIncomingUser)
 }
